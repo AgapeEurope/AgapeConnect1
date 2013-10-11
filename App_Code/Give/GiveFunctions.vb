059dff4b6c3ff0572d26cca11f50cb64f4f108cc
David VIOLLEAU <david.violleau@agapefrance.org>
2013-10-11 18:44:49 +0200

    AgapeFR Skin Giving page + Reset CSS + Giving Functions code

--- App_Code/Give/GiveFunctions.vb
+++ App_Code/Give/GiveFunctions.vb
@@ -6,6 +6,21 @@ Imports StaffBroker
 
 #Region "Modules defining constant values"
 
+' List of values for giving type (used for URL parameters)
+Public Module GiveType
+    Public Const Staff As Integer = 0
+    Public Const Dept As Integer = 1
+    Public Const Project As Integer = 2
+    Public Function GetName(ByVal GiveTypeNumber As Integer) As String
+        Select Case GiveTypeNumber
+            Case 0 : Return "Staff"
+            Case 1 : Return "Dept"
+            Case 2 : Return "Project"
+            Case Else : Return "Unknown"
+        End Select
+    End Function
+End Module
+
 #End Region ' Modules defining constant values
 
 Public Class GiveFunctions
