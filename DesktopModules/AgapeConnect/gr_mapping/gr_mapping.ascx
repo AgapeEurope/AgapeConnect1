6d4f38ca67bee7034841bda7dfd294a21101b884
Jon Vellacott <JonVellacott@agape.org.uk>
2014-02-18 13:57:06 +0000

    Created/implemented grMapping module

--- DesktopModules/AgapeConnect/gr_mapping/gr_mapping.ascx
+++ DesktopModules/AgapeConnect/gr_mapping/gr_mapping.ascx
@@ -1,12 +1,79 @@
-﻿<%@ Control Language="VB" AutoEventWireup="False" CodeFile="gr_mapping.ascx.vb" Inherits="DotNetNuke.Modules.AgapeConnect.gr_mapping" %>
+﻿<%@ Control Language="VB" AutoEventWireup="False" CodeFile="gr_mapping.ascx.vb" Inherits="DotNetNuke.Modules.AgapeConnect.gr_mapping_mod" %>
 <%@ Register TagPrefix="dnn" TagName="Label" Src="~/controls/LabelControl.ascx" %>
+<style type="text/css">
+   .LocalType {
+    font-size: small;
+    color: lightgray;
+    font-style: italic;
+    text-align: right;
+    width: 90px;
+    float: left;
+    margin-right: 5px;
+}
+   .mappingTitle{
+       font-weight: bold;
+   }
 
-Global Registry API Key : <asp:TextBox ID="tbApiKey" runat="server" width ="400px"></asp:TextBox><br />
+</style>
+
+<p>
+Global Registry API Key : <asp:TextBox ID="tbApiKey" runat="server" width ="400px"></asp:TextBox>
+</p>
+<fieldset>
+    <legend><h3>Field Mappings</h3></legend>
+
+    <p>This section allows you to configure how fields on this webportal map to fields in the Global Registry.
+        Once a day, user information from this website is pumped into the Global Registry using the following mappings.
+    </p>
+
+
+<asp:GridView ID="gvMappings" runat="server" BackColor="White" BorderColor="#DEDFDE" BorderStyle="None" BorderWidth="1px" CellPadding="4" ForeColor="Black" GridLines="Vertical" AutoGenerateColumns="False" >
+    <AlternatingRowStyle BackColor="White" />
+    <Columns>
+        <asp:TemplateField HeaderText="Local Field" ItemStyle-Width="300px">
+            <EditItemTemplate>
+                <asp:TextBox ID="TextBox1" runat="server" Text='<%# Bind("LocalName") %>'></asp:TextBox>
+            </EditItemTemplate>
+            <ItemTemplate>
+                <div class="LocalType"><asp:Label ID="Label2" runat="server"  Text='<%# getLocalTypeName( Eval("LocalSource")) %>'></asp:Label></div>
+                <asp:Label ID="Label1" runat="server" Text='<%# Bind("LocalName") %>'></asp:Label>
+            </ItemTemplate>
+        </asp:TemplateField>
+        <asp:TemplateField>
+            <ItemTemplate>
+                -->
+            </ItemTemplate>
+        </asp:TemplateField>
+        <asp:BoundField DataField="gr_dot_notated_name" HeaderText="Global Registry Field" ItemStyle-Width="300px" />
+        <asp:TemplateField ShowHeader="False">
+            <ItemTemplate>
+                <asp:LinkButton ID="LinkButton1" runat="server" CausesValidation="False" CommandName="myDelete" Text="Delete" CommandArgument='<%# Eval("ID") %>'></asp:LinkButton>
+            </ItemTemplate>
+        </asp:TemplateField>
+    </Columns>
+    <FooterStyle BackColor="#CCCC99" />
+    <HeaderStyle BackColor="#6B696B" Font-Bold="True" ForeColor="White" />
+    <PagerStyle BackColor="#F7F7DE" ForeColor="Black" HorizontalAlign="Right" />
+    <RowStyle BackColor="#F7F7DE" />
+    <SelectedRowStyle BackColor="#CE5D5A" Font-Bold="True" ForeColor="White" />
+    <SortedAscendingCellStyle BackColor="#FBFBF2" />
+    <SortedAscendingHeaderStyle BackColor="#848384" />
+    <SortedDescendingCellStyle BackColor="#EAEAD3" />
+    <SortedDescendingHeaderStyle BackColor="#575357" />
+</asp:GridView>
+    <div class="mappingTitle">Add new mapping:</div>
+    <table cellspacing="2px">
+        
+        <tr>
+            <td width="310px"><asp:DropDownList ID="ddlProfileMap" runat="server" Width="100%" ></asp:DropDownList></td>
+            <td>--></td>
+            <td width="310px"><asp:DropDownList ID="gr_entity_types" runat="server" Width="100%"  ></asp:DropDownList></td>
+            <td><asp:LinkButton ID="btnAdd" runat="server">add</asp:LinkButton></td>
+        </tr>
+    </table>
 
 
 
-<asp:GridView ID="gvMappings" runat="server" DataSourceID="dsGR_Mappings"></asp:GridView>
 
 
-
-<asp:ListBox ID="gr_entity_types" runat="server" Height="400px"></asp:ListBox>
+    </fieldset>
--- DesktopModules/AgapeConnect/gr_mapping/gr_mapping.ascx.vb
+++ DesktopModules/AgapeConnect/gr_mapping/gr_mapping.ascx.vb
@@ -15,7 +15,7 @@ Imports DotNetNuke.Security
 Imports GR_NET.GR.NET
 Imports gr_mapping
 Namespace DotNetNuke.Modules.AgapeConnect
-    Partial Class gr_mapping
+    Partial Class gr_mapping_mod
         Inherits Entities.Modules.PortalModuleBase
 
 
@@ -23,32 +23,126 @@ Namespace DotNetNuke.Modules.AgapeConnect
 
         Dim gr As GR
 
+        Private Sub ResetLocalDDL()
+            Dim ds As New StaffBroker.StaffBrokerDataContext
+            ddlProfileMap.Items.Clear()
+            ddlProfileMap.Items.Add(New ListItem("User: Email", "U-Email"))
+            ddlProfileMap.Items.Add(New ListItem("User: DisplayName", "U-DisplayName"))
+            ddlProfileMap.Items.Add(New ListItem("User: FirstName", "U-FirstName"))
+            ddlProfileMap.Items.Add(New ListItem("User: LastName", "U-LastName"))
 
-        Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
-            tbApiKey.Text = StaffBrokerFunctions.GetSetting("gr_api_key", PortalId)
+           
 
-            Dim d As New gr_mappingDataContext
-            Dim mappings = From c In d.gr_mappings Where c.PortalId = PortalId
+            Dim ups = From c In ds.ProfilePropertyDefinitions Where c.PortalID = PortalId Order By c.PropertyName Select c.PropertyName, c.PropertyDefinitionID
 
-            gvMappings.DataSource = mappings
-            gvMappings.DataBind()
+            For Each row In ups
+                ddlProfileMap.Items.Add(New ListItem("User Profile: " & row.PropertyName, "UP-" & row.PropertyDefinitionID))
 
+            Next
 
-
-            If Not tbApiKey.Text = "" Then
-                gr = New GR(tbApiKey.Text, "https://gr.stage.uscm.org/")
-                Dim leaves = From c In gr.GetFlatEntityLeafList("person") Select Name = c.GetDotNotation, c.ID Order By Name
+            ddlProfileMap.Items.Add(New ListItem("Staff: R/C", "S-CostCenter"))
 
 
-                gr_entity_types.DataSource = leaves
-                gr_entity_types.DataTextField = "Name"
-                gr_entity_types.DataValueField = "ID"
-                gr_entity_types.DataBind()
-            End If
+            Dim sps = From c In ds.AP_StaffBroker_StaffPropertyDefinitions Order By c.PropertyName Where c.PortalId = PortalId
+            For Each row In sps
+                ddlProfileMap.Items.Add(New ListItem("Staff Profile: " & row.PropertyName, "SP-" & row.StaffPropertyDefinitionId))
 
+            Next
 
         End Sub
 
 
+        Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+           
+            If Not Page.IsPostBack Then
+
+
+                tbApiKey.Text = StaffBrokerFunctions.GetSetting("gr_api_key", PortalId)
+
+                Dim d As New gr_mappingDataContext
+                Dim mappings = From c In d.gr_mappings Where c.PortalId = PortalId
+
+                gvMappings.DataSource = mappings
+                gvMappings.DataBind()
+
+              
+                ResetLocalDDL()
+               
+
+
+                If Not tbApiKey.Text = "" Then
+                    gr = New GR(tbApiKey.Text, "https://gr.stage.uscm.org/")
+
+
+
+                    Dim leaves = From c In gr.GetFlatEntityLeafList("person") Select Name = c.GetDotNotation, c.ID Order By Name
+
+
+                    gr_entity_types.DataSource = leaves
+                    gr_entity_types.DataTextField = "Name"
+                    gr_entity_types.DataValueField = "ID"
+                    gr_entity_types.DataBind()
+                End If
+
+            End If
+        End Sub
+
+
+        Protected Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+            Dim d As New gr_mappingDataContext
+            Dim q = From c In d.gr_mappings Where c.PortalId = PortalId And c.LocalName = ddlProfileMap.SelectedItem.Text _
+              And c.LocalSource = "UP" And c.gr_dot_notated_name = gr_entity_types.SelectedItem.Text
+
+            If q.Count = 0 Then
+                Dim insert As New gr_mapping.gr_mapping()
+                insert.PortalId = PortalId
+                insert.LocalName = ddlProfileMap.SelectedItem.Text.Substring(ddlProfileMap.SelectedItem.Text.IndexOf(":") + 2)
+                insert.LocalSource = ddlProfileMap.SelectedValue.Substring(0, ddlProfileMap.SelectedValue.IndexOf("-"))
+                insert.gr_dot_notated_name = gr_entity_types.SelectedItem.Text
+                insert.FieldType = "string"
+                d.gr_mappings.InsertOnSubmit(insert)
+                d.SubmitChanges()
+
+                Dim mappings = From c In d.gr_mappings Where c.PortalId = PortalId
+
+                gvMappings.DataSource = mappings
+                gvMappings.DataBind()
+
+            End If
+
+           
+
+        End Sub
+
+        Protected Sub gvMappings_RowCommand(sender As Object, e As GridViewCommandEventArgs) Handles gvMappings.RowCommand
+            If e.CommandName = "myDelete" Then
+                Dim d As New gr_mappingDataContext
+                Dim q = From c In d.gr_mappings Where c.Id = CInt(e.CommandArgument)
+
+                d.gr_mappings.DeleteAllOnSubmit(q)
+                d.SubmitChanges()
+                Dim mappings = From c In d.gr_mappings Where c.PortalId = PortalId
+
+                gvMappings.DataSource = mappings
+                gvMappings.DataBind()
+
+            End If
+        End Sub
+        Public Function getLocalTypeName(ByVal LocalType As String) As String
+            Select Case LocalType
+                Case "UP"
+                    Return "User Profile:"
+                Case "U"
+                    Return "User:"
+                Case "SP"
+                    Return "Staff Profile:"
+                Case "S"
+                    Return "Staff"
+
+                Case Else
+                    Return ""
+            End Select
+        End Function
     End Class
 End Namespace
